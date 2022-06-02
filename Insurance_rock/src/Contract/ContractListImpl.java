c61b7885b4c872a11c402a090d5aac5da762911b
duekdms <mmp7afy8@naver.com>
2022-06-02 23:40:02 +0900

    dd

--- Insurance_rock/src/Contract/ContractListImpl.java
+++ Insurance_rock/src/Contract/ContractListImpl.java
@@ -18,48 +18,15 @@ import dao.ContractDao;
 public class ContractListImpl implements ContractList {
 	private ArrayList<Contract> contractList = new ArrayList<Contract>();
 	private Contract contract;
-	private ContractDao contractDAO;
 	
 
 	public ContractListImpl(){
 		this.contract = new Contract();
-		this.contractDAO = new ContractDao();
 //		this.setContract();
 
 	}
 	
 
-	private void setContract() {	
-		ResultSet resultSet = contractDAO.retrive();
-		
-		try {
-			while (resultSet.next()) {
-				Contract contract = new Contract();
-				contract.setContractID(resultSet.getString("contractID"));
-				contract.setCustomerID(resultSet.getString("customerID"));
-				contract.setCustomerName(resultSet.getString("customerName"));
-				contract.setPhoneNum(resultSet.getString("customerPhoneNum"));
-				contract.setInsuranceID("insuranceID");
-				contract.setInsuranceName(resultSet.getString("insuranceName"));
-				contract.setPaymentCycle(resultSet.getInt("paymentCycle"));
-				contract.setInsuranceFee(resultSet.getInt("insuranceFee"));
-				contract.setUnpaidFee(resultSet.getInt("unpaidFee"));
-				contract.setSecurityFee(resultSet.getInt("securityFee"));
-				contract.setProvisionFee(resultSet.getInt("provisionFee"));
-				contract.setStartDate(LocalDate.parse(resultSet.getString("startDate")));
-				contract.setEndDate(LocalDate.parse(resultSet.getString("endDate")));
-				
-				contractList.add(contract);
-			}
-		} catch (SQLException e) {
-			System.out.println(
-					"파일 접근 중 문제가 생겨 계약정보를 불러오지 못했습니다. 잠시후 다시 실행 해주십시오. 해당 문제가 계속 발생할 시에는 사내 시스템 관리팀(1234-5678)에게 문의 주시기 바랍니다.");
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		// TODO Auto-generated method stub
-
-	}
 	
 
 	public void finalize() throws Throwable {
